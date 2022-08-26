5f541c7b80a2858cc06aea4dc2a6d632a74ade4a
Juan Hernandez <55467194+ElChetoMDQ97@users.noreply.github.com>
2022-08-25 21:21:39 -0300

    Update favicon.js

--- api/src/routes/favicon.js
+++ api/src/routes/favicon.js
@@ -1,1 +1,13 @@
+const { Router } = require("express");
+const fetch = require("node-fetch");
+const { Tipo } = require("../db.js");
 
+const router = Router();
+
+router.get('/', async (req, res) => {
+    res.json({info: "favicon"});
+})
+
+
+
+module.exports = router;
