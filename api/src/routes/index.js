0183d6540faec8b5497e742dc6f5b9fc56adbe98
Juan Hernandez <55467194+ElChetoMDQ97@users.noreply.github.com>
2022-08-25 21:19:34 -0300

    Update index.js

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,14 +3,15 @@ const { Router } = require('express');
 // Ejemplo: const authRouter = require('./auth.js');
 const pokemons = require('./pokemons.js');
 const types = require('./types.js');
-
+const favicon = require('./favicon.js');
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 router.use('/pokemons', pokemons);
-router.use('/types', types)
+router.use('/types', types);
+router.use('/favicon.ico', favicon)
 
 
 module.exports = router;
